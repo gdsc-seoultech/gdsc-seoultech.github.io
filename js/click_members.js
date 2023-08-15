b39d9304f07d38ea6102ebbf137ea32b3e464763
Hong-Mu <hongmuchae@gmail.com>
2023-08-15 09:59:45 +0900

    멤버 페이지 모달 로직 수정

--- js/click_members.js
+++ js/click_members.js
@@ -1,87 +1,33 @@
 window.onload = function () {
-  const url = window.location.href;
-  let term = url.substring(url.lastIndexOf('/') + 1);
-  term = Number(term.replace(/[^0-9]/g, "")) - 1; // 숫자가 아닌 문자열을 매칭하는 정규식
-  const idList = [
-      ["su",
-        "min",
-        "seong",
-        "ye",
-        "in",
-        "ui",
-        "goldtan",
-        "yh",
-        "keonju2",
-        "jisoo",
-        "shinyubin989",
-        "yongsu",
-        "pathpioneer",
-        "jungin",
-        "ehrwk",
-        "drizzle",
-        "leeeha",
-        "jih",
-        "yoonjae"],
-      [
-        "drizzle",
-        "keonju",
-        "yejin",
-        "yuseogi",
-        "kangmin",
-        "leeeha",
-        "juwhan",
-        'dann',
-        'hongmu',
-        'jwyeeh-dev',
-         "yoouung",
-         "Higeuni",
-         "seunggu",
-         "chunngye",
-         "gagyeong",
-         "hyunjun",
-         "jonghun",
-         "hyeok",
-         "yeonsu",
-         "songhee",
-         "jeongjoon",
-         "dongyoung",
-         "junseok",
-         "minseob",
-      ]
 
-  ];
-
-
-
-  idList[term].map((item, index) => {
-    function onClick() {
-      idList[term].map((item, index) => {
-        document.querySelectorAll(".modal_wrap")[index].style.display = "none";
-        document.querySelectorAll(".black_bg")[index].style.display = "none";
-      });
-      document.querySelectorAll(".modal_wrap")[index].style.display = "block";
-      document.querySelectorAll(".black_bg")[index].style.display = "block";
-    }
-
-    function offClick() {
-      document.querySelectorAll(".modal_wrap")[index].style.display = "none";
-      document.querySelectorAll(".black_bg")[index].style.display = "none";
-    }
-
-    document.getElementById(item).addEventListener("click", onClick);
-    document.querySelectorAll(".modal_close")[index].addEventListener("click", offClick);
-  });
-
-  function offAll() {
-    idList[term].map((item, index) => {
-      document.querySelectorAll(".modal_wrap")[index].style.display = "none";
-      document.querySelectorAll(".black_bg")[index].style.display = "none";
-    });
+  function closeAll() {
+    document.querySelectorAll(".modal_wrap, .black_bg").forEach((element) => {
+      element.style.display = "none";
+    })
   }
+
+  document.querySelectorAll(".member-card-container").forEach( (part) => {
+
+    part.querySelectorAll(".member-card > .folder").forEach( (member, idx) => {
+      member.addEventListener("click", () => {
+        closeAll()
+        part.querySelectorAll(".modal_wrap")[idx].style.display = "block";
+        part.querySelectorAll(".black_bg")[idx].style.display = "block";
+      });
+    });
+
+    part.querySelectorAll(".modal_close").forEach( (member, idx) => {
+      member.addEventListener("click", () => {
+        part.querySelectorAll(".modal_wrap")[idx].style.display = "none";
+        part.querySelectorAll(".black_bg")[idx].style.display = "none";
+      });
+    });
+
+  });
 
   window.onkeydown = function (event) {
     if (event.keyCode === 27) {
-      offAll();
+      closeAll()
     }
   };
 };
