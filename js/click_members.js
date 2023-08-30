0e117dd89688176ec48ef908a209f56c27e780cb
changha <mentenseoul@gmail.com>
2023-08-30 15:51:18 +0900

    [FIX] add modal-bg click

--- js/click_members.js
+++ js/click_members.js
@@ -11,6 +11,7 @@ window.onload = function () {
     part.querySelectorAll(".member-card > .folder").forEach( (member, idx) => {
       member.addEventListener("click", () => {
         closeAll()
+        part.querySelectorAll(".modal_bg")[idx].style.display = "block";
         part.querySelectorAll(".modal_wrap")[idx].style.display = "block";
         part.querySelectorAll(".black_bg")[idx].style.display = "block";
       });
@@ -18,11 +19,19 @@ window.onload = function () {
 
     part.querySelectorAll(".modal_close").forEach( (member, idx) => {
       member.addEventListener("click", () => {
+        part.querySelectorAll(".modal_bg")[idx].style.display = "none";
         part.querySelectorAll(".modal_wrap")[idx].style.display = "none";
         part.querySelectorAll(".black_bg")[idx].style.display = "none";
       });
     });
-
+    
+    part.querySelectorAll(".modal_bg").forEach( (member, idx) => {
+      member.addEventListener("click", () => {
+        part.querySelectorAll(".modal_bg")[idx].style.display = "none";
+        part.querySelectorAll(".modal_wrap")[idx].style.display = "none";
+        part.querySelectorAll(".black_bg")[idx].style.display = "none";
+      });
+    });
   });
 
   window.onkeydown = function (event) {
